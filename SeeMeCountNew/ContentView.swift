3939fb7508211b76a1e2093b94958157f032616b
Ian Ellis <ellisiana@gmail.com>
2023-02-01 21:49:12 +0000

    Change background to black

--- SeeMeCountNew/ContentView.swift
+++ SeeMeCountNew/ContentView.swift
@@ -9,91 +9,96 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        
-        GeometryReader { geometry in
-            // Access the width and height of the view
-            let width = geometry.size.width
-            let height = geometry.size.height
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
             
-            // Row 1
-            let x1 = CGFloat(100)
-            let y1 = CGFloat(100)
-
-            let x2 = (height / 2)
-            let y2 = y1
-
-            let x3 = height - 100
-            let y3 = y1
-
-            let x4 = (1.5 * height) - 200
-            let y4 = y1
-
-            
-            Image("No1")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x1 ,y: y1)
-
-            Image("No2")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x2 ,y: y2)
-
-            Image("No3")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x3 ,y: y3)
-
-            Image("No4")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x4 ,y: y4)
-
-            // Row 3 (defined before Row 2)
-            let x7 = (x1 + x2) / 2
-            let y7 = width / 1.5
-
-            let x8 = (x2 + x3) / 2
-            let y8 = y7
-
-            let x9 = (x3 + x4) / 2
-            let y9 = y7
-
-            Image("No7")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x7 ,y: y7)
-
-            Image("No8")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x8 ,y: y8)
-
-            Image("No9")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x9 ,y: y9)
-            
-            // Row 2
-            let x5 = x1
-            let y5 = (y1 + y9) / 2
-
-            let x6 = x4
-            let y6 = y5
-
-            Image("No5")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x5 ,y: y5)
-
-            Image("No6")
-                .resizable()
-                .frame( width: 100, height: 100)
-                .position(x:x6 ,y: y6)
-
+            GeometryReader { geometry in
+                // Access the width and height of the view
+                let width = geometry.size.width
+                let height = geometry.size.height
+                
+                // Row 1
+                let x1 = CGFloat(100)
+                let y1 = CGFloat(100)
+    
+                let x2 = (height / 2)
+                let y2 = y1
+    
+                let x3 = height - 100
+                let y3 = y1
+    
+                let x4 = (1.5 * height) - 200
+                let y4 = y1
+                
+                
+                Image("No1")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x1 ,y: y1)
+    
+                Image("No2")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x2 ,y: y2)
+    
+                Image("No3")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x3 ,y: y3)
+    
+                Image("No4")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x4 ,y: y4)
+    
+                // Row 3 (defined before Row 2)
+                let x7 = (x1 + x2) / 2
+                let y7 = width / 1.5
+    
+                let x8 = (x2 + x3) / 2
+                let y8 = y7
+    
+                let x9 = (x3 + x4) / 2
+                let y9 = y7
+    
+                Image("No7")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x7 ,y: y7)
+    
+                Image("No8")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x8 ,y: y8)
+    
+                Image("No9")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x9 ,y: y9)
+                
+                // Row 2
+                let x5 = x1
+                let y5 = (y1 + y9) / 2
+    
+                let x6 = x4
+                let y6 = y5
+    
+                Image("No5")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x5 ,y: y5)
+    
+                Image("No6")
+                    .resizable()
+                    .frame( width: 100, height: 100)
+                    .position(x:x6 ,y: y6)
+    
+            }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
